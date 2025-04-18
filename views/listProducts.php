bcd25a46ee239e5df13b1de245a6c3a87b6366d2
kayle-henry <103394438+kayle-henry@users.noreply.github.com>
2025-04-18 18:31:33 -0500

    Update listProducts.php

--- views/listProducts.php
+++ views/listProducts.php
@@ -3,30 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CS 2033 | Products Shop[</title>
+    <title>CS 2033 | Products Database</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-light bg-light" style="margin-bottom: 20px">
-    <a class="navbar-brand" href="listProducts.php">
-        Product Shop
-    </a>
-    </nav>
+    <h1><center>Product Database</center></h1>
     <div class="container">
         <div class="col">
             <form action="controller.php" method="GET">
             <button class="btn btn-primary" type="submit" name="page" value="add">Add Product</button>
-            <button class="btn btn-primary" type="submit" name="page" value="delete">Update Product</button>
+            <button class="btn btn-primary" type="submit" name="page" value="update">Update Product Price</button>
             <table class="table table-bordered table-striped">
-                <thead><tr><th>Product ID</th><th>Category ID</th><th>Product Code</th><th>Product Name</th><th>Price</th></tr></thead>
+                <thead>
+                    <tr>
+                        <th>Select</th>
+                        <th>Product ID</th>
+                        <th>Category ID</th>
+                        <th>Product Code</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                    </tr>    
+                </thead>
                 <tbody>
                     <?php
+                        $products = $productDAO->getProducts();
+                        if (empty($products)) {
+                            echo "<tr><td colspan='5'>No products found.</td></tr>";
+                        }else{
+                            //echo "<tr><td colspan='5'>".count($products)." products found.</td></tr>";
+                        }
                         for($index=0;$index<count($products);$index++){
-                            echo "<tr><td><input type=\"radio\" name=\"contactID\" value=\"".$products[$index]->getProductID()."\"></td>";
+                            echo "<tr>";
+                            echo "<td><input type=\"radio\" name=\"productID\" value=\"".$products[$index]->getProductID()."\"></td>";
                             echo "<td>".$products[$index]->getProductID()."</td>";
                             echo "<td>".$products[$index]->getCategoryID()."</td>";
+                            echo "<td>".$products[$index]->getProductCode()."</td>";
                             echo "<td>".$products[$index]->getProductName()."</td>";
-                            echo "<td>".$products[$index]->getListPrice()."</td></tr>";
+                            echo "<td>".$products[$index]->getListPrice()."</td>";
+                            echo "</tr>";
                         }
                     ?>
                 </tbody>        
