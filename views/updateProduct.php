924d85d353274f2908ad6fa142dd7206b6472b99
kayle-henry <103394438+kayle-henry@users.noreply.github.com>
2025-04-18 18:32:01 -0500

    Update updateProduct.php

--- views/updateProduct.php
+++ views/updateProduct.php
@@ -7,24 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-
-    <nav class="navbar navbar-light bg-light" style="margin-bottom: 20px">
-    <a class="navbar-brand" href="listProducts.php">
-        CS 2033 Web Systems Model-View-Controller (MVC)
-    </a>
-    </nav>
     <div class="container">
         <div class="row">
             <div class="col">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Product List</h5>
-                        <p class="card-text">Update the price of a product.</p>
+                        <p class="card-text">Update the price of 
+                        <?php
+                        echo " (Product ID: " . htmlspecialchars($_GET['productID']) . ")";
+                        ?>    
+                        .</p>
                         <form action="controller.php" method="POST">
                             <input type="hidden" name="page" value="update">
                             <label for="listPrice" class="form-label">List Price</label>
                             <input type="text" class="form-control mb-3" id="listPrice" name="listPrice" placeholder="Enter the product price" required>
                             <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>   
+                        <form action="controller.php" method="GET">
+                            <input type="hidden" name="page" value="list">
+                            <button type="submit" class="btn btn-secondary">Cancel</button>
                         </form>
                     </div>
                 </div>      
