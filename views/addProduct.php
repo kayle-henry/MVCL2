19afccdd8b91f8af88438fb5a41b55eb60b3e374
kayle-henry <103394438+kayle-henry@users.noreply.github.com>
2025-04-19 15:07:12 -0500

    Update addProduct.php

--- views/addProduct.php
+++ views/addProduct.php
@@ -39,27 +39,3 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-<input type="hidden" name="page" value="add">
-                            <label for="categoryID" class="form-label">categoryID</label>
-                            <input type="text" class="form-control mb-3" id="categoryID" name="categoryID" placeholder="Enter the category ID" required>
-                            <label for="productCode" class="form-label">Product Code</label>
-                            <input type="text" class="form-control mb-3" id="productCode" name="productCode" placeholder="Enter the product code" required>
-                            <label for="productName" class="form-label">Product Name</label>
-                            <input type="text" class="form-control mb-3" id="productName" name="productName" placeholder="Enter the product name" required>
-                            <label for="listPrice" class="form-label">List Price</label>
-                            <input type="text" class="form-control mb-3" id="listPrice" name="listPrice" placeholder="Enter the product price" required>
-                            <button type="submit" class="btn btn-primary">Submit</button>  
-
-                            <form action="controller.php" method="GET">
-                            <input type="hidden" name="page" value="list">
-                            <button type="submit" class="btn btn-secondary">Cancel</button>
-                            </form>
-                    </div>
-                </div>      
-            </div>
-        </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
