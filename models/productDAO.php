3d5a1df78f42814f6f5352894615afbdee28d413
kayle-henry <103394438+kayle-henry@users.noreply.github.com>
2025-04-20 17:02:08 -0500

    Update productDAO.php

--- models/productDAO.php
+++ models/productDAO.php
@@ -12,12 +12,20 @@
         }
 
         public function addProduct($product){
+            $categoryID = $product->getCategoryID();
+            $productCode = $product->getProductCode();
+            $productName = $product->getProductName();
+            $listPrice = $product->getListPrice();
+
+
             $connection=$this->getConnection();
             $stmt = $connection->prepare("INSERT INTO products (categoryID, productCode, productName, listPrice) VALUES (?, ?, ?, ?)");
-            $stmt->bind_param("issd",$product->getCategoryID(),$product->getProductCode(), $product->getProductName(), $product->getListPrice());
+            $stmt->bind_param("issd",$categoryID, $productCode, $productName, $listPrice); // "i" for integer (categoryID), "s" for string (productCode, productName), "d" for double (listPrice)
             $stmt->execute();
+            $productID = $connection->insert_id;
             $stmt->close();
             $connection->close();
+            return $productID;
         }
 
         public function deleteProduct($productID){
@@ -58,6 +66,17 @@
             $stmt->close();
             $connection->close();
             return $products;
+        }
+        public function getProductNameByID($productID) { //used in updateProduct for UI
+            $connection = $this->getConnection();
+            $stmt = $connection->prepare("SELECT productName FROM products WHERE productID = ?");
+            $stmt->bind_param("i", $productID);
+            $stmt->execute();
+            $stmt->bind_result($productName);
+            $stmt->fetch();
+            $stmt->close();
+            $connection->close();
+            return $productName;
         }
     }
 ?>
