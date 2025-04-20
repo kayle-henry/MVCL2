890be1349fee38a0f1c9a45d14cf53968c923f0f
kayle-henry <103394438+kayle-henry@users.noreply.github.com>
2025-04-20 17:03:57 -0500

    Update addProduct.php

--- views/addProduct.php
+++ views/addProduct.php
@@ -6,6 +6,11 @@
     <title>CS 2033 | Add Product</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+<style>
+    .container {
+        padding-top: 20px;
+    }
+</style>
 <body>
 
     <div class="container">
@@ -13,8 +18,8 @@
             <div class="col">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Product List</h5>
-                        <p class="card-text">Add a new product to the list.</p>
+                        <h5 class="card-title">Product Database</h5>
+                        <p class="card-text">Add a new product to the database.</p>
                         <form action="controller.php" method="POST">
                             <input type="hidden" name="page" value="add">
                             <label for="categoryID" class="form-label">categoryID</label>
@@ -25,12 +30,14 @@
                             <input type="text" class="form-control mb-3" id="productName" name="productName" placeholder="Enter the product name" required>
                             <label for="listPrice" class="form-label">List Price</label>
                             <input type="text" class="form-control mb-3" id="listPrice" name="listPrice" placeholder="Enter the product price" required>
+                        <div class="d-flex gap-2 mt-3">
                             <button type="submit" class="btn btn-primary">Submit</button>  
-
+                        </form>
                             <form action="controller.php" method="GET">
-                            <input type="hidden" name="page" value="list">
-                            <button type="submit" class="btn btn-secondary">Cancel</button>
+                                <input type="hidden" name="page" value="list">
+                                <button type="submit" class="btn btn-secondary">Cancel</button>
                             </form>
+                        </div>
                     </div>
                 </div>      
             </div>
@@ -39,3 +46,4 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+</html>
